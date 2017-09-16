39f1d9b559ee1b548e2eb4fbd74a6a3707c06c8b
programmer76 <programmer76@yandex.ru>
2017-09-16 20:27:46 +0300

    1

---
+++ labQueue/Controllers/HomeController.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+using labQueue.Models;
+using System.Collections;
+
+namespace labQueue.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            //динамически создавать массив талонов на текущий день
+            DateTime dt = DateTime.Today.AddHours(8);
+            int curTalonNumber = 1;
+
+            ArrayList talons = new ArrayList();
+            for(int i=0; i<16; i++)
+            {
+                for(int j=0; j < 20; j++)
+                {
+                    Talon talon = new Talon();
+                    talon.talonNumber = curTalonNumber;
+                    talon.talonDateTimePeriod = dt;
+                    talon.talon_status = Talon.talon_statuses.свободен;
+                    talons.Add(talon);
+                    curTalonNumber++;
+                }
+                dt = dt.AddMinutes(15);
+            }
+            
+            ViewBag.Message = "Your application description page.";
+            ViewBag.Message17 = "Your application description page 17.";
+            ViewBag.talons = talons;
+            ViewBag.timePeriods = 16;
+            ViewBag.talonsInPeriod = 20;
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
---
+++ labQueue/Models/Talon.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace labQueue.Models
+{
+    public class Talon
+    {
+        public enum talon_statuses  {свободен,недоступен,вызов,завершен};
+
+        public int talonNumber = 1;
+        public DateTime talonDateTimePeriod { get; set; }
+        public talon_statuses talon_status { get; set; }
+
+    }
+}
